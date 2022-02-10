7927607798eed1116a9b8d67dfbae905c1cf363f
luis Quevedo <qfluis@gmail.com>
2022-02-10 10:29:59 +0100

    Ejercicio 1 finalizado

---
+++ src/app/components/escena/escena.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-escena',
+  templateUrl: './escena.component.html',
+  styleUrls: ['./escena.component.scss']
+})
+export class EscenaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
