d3f3c658d93326c34718d1cf475e603fd5f24817
luis Quevedo <qfluis@gmail.com>
2022-02-10 11:00:57 +0100

    Ejercicio 2 Finalizado

--- src/app/components/escena/escena.component.ts
+++ src/app/components/escena/escena.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-escena',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EscenaComponent implements OnInit {
 
+  @Input() data:string[] =[];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  historia:string [];
+
+  constructor() { 
+    this.historia = ["El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+    "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+    "L'heroi va decidir travessar la porta que el portava a casa",
+    "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."];
+  }
 
   ngOnInit(): void {
   }
