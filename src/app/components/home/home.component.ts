5972c2ca1ac33032dd77751d3a85b335e0256bb5
luis Quevedo <qfluis@gmail.com>
2022-02-10 12:16:11 +0100

    Ejercicio 5 Finalizado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,12 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   historia:string [];
+  iniciado:boolean;
 
   constructor() { 
     this.historia = ["El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
     "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
     "L'heroi va decidir travessar la porta que el portava a casa",
     "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."];
+    this.iniciado = false;
+  }
+
+  iniciar(){
+    this.iniciado=true;
   }
 
   ngOnInit(): void {
